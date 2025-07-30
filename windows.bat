79ab5ce5b5c9140e15f7a812a97a44b2f03455ab
landonslanding <158111088+landonslanding@users.noreply.github.com>
2025-07-29 18:53:05 -0700

    Add files via upload

---
+++ windows.bat
@@ -0,0 +1,6 @@
+rem Set the content of the eicar file
+set "content=X5O!P%%@AP[4\PZX54(P^^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+
+echo %content% > %CD%\eicar.com
+
+REM 2025-07-30 01:00:09.219621
